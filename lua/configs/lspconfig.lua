876e5adce50abc50277040b66feecf36856b78e1
jtzero ? <jtzero511@gmail.com>
2024-09-09 19:43:19 -0400

    fix for commmit c7b1147 breaking icons

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -22,3 +22,24 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+--
+
+-- Icons broke on c7b1147
+local function lspSymbol(name, icon)
+  local hl = "DiagnosticSign" .. name
+  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
+
+lspSymbol("Error", "󰅙")
+lspSymbol("Info", "󰋼")
+lspSymbol("Hint", "󰌵")
+lspSymbol("Warn", "")
+
+vim.diagnostic.config {
+  virtual_text = {
+    prefix = "",
+  },
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+}
