860bff07d5379c3ee3929d6370669cf4dd034a5c
jtzero ? <jtzero511@gmail.com>
2024-09-09 20:34:14 -0400

    fixes symbol highlighting in ruby

--- lua/options.lua
+++ lua/options.lua
@@ -11,3 +11,5 @@ vim.cmd('source ~/.vimrc-init')
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 --
+
+vim.api.nvim_set_hl(0, '@string.special.symbol.ruby', { fg = '#f1f18c' })
