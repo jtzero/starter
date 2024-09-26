c3cd8676b089b8418d5748c3467f56451d229951
jtzero ? <jtzero511@gmail.com>
2024-09-26 15:15:50 -0400

    makes symbol highlight group dynamic based on theme

--- lua/options.lua
+++ lua/options.lua
@@ -12,4 +12,16 @@ vim.cmd('source ~/.vimrc-init')
 -- o.cursorlineopt ='both' -- to enable cursorline!
 --
 
-vim.api.nvim_set_hl(0, '@string.special.symbol.ruby', { fg = '#f1f18c' })
+-- doesn;t work in the theme preview for some reason
+vim.api.nvim_create_autocmd({"BufEnter", "BufRead"}, {
+  callback = function()
+    local theme = require("nvconfig").ui.theme
+    if theme == "chadracula" then
+      vim.api.nvim_set_hl(0, '@string.special.symbol.ruby', { fg = '#f1f18c' })
+    --elseif theme == 'onenord_light' then
+    -- only works for in a gui, for iterm2 use "Cursor Colors" > "Smart box Cursor color"
+    --  vim.api.nvim_set_hl(0, "Cursor", {fg='black', bg='black'})
+    end
+  end,
+  --group = my_buf_enter_group,
+})
