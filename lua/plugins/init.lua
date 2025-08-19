2100f7d3e0ed2d5968e89070b535b423c36e4f99
jtzero ? <jtzero511@gmail.com>
2025-08-18 20:10:42 -0400

    stop auto folding on astro

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -202,7 +202,7 @@ return {
 				-- INFO some filetypes only allow indent, some only LSP, some only
 				-- treesitter. However, ufo only accepts two kinds as priority,
 				-- therefore making this function necessary :/
-				local lspWithOutFolding = { "markdown", "sh", "css", "html", "python" }
+				local lspWithOutFolding = { "markdown", "sh", "css", "html", "python", "astro" }
 				if vim.tbl_contains(lspWithOutFolding, ft) then return { "treesitter", "indent" } end
 				return { "lsp", "indent" }
 			end,
